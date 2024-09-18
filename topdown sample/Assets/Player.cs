290024c7d95d738e8c4aa09b7eec42980b2aad90
AJSGOTTHEWAY <ajm4559@rit.edu>
2024-09-17 23:02:19 -0400

    SampleGame

---
+++ topdown sample/Assets/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 velocity;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = Vector2.zero;
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(velocity);
+        UpdateAnimation();
+    }
+
+
+    void UpdateAnimation()
+    {
+
+        if(velocity != Vector2.zero)
+        {
+
+            UpdateMovement();
+
+            animator.SetBool("Walking", true);
+            animator.SetFloat("Horizontal", velocity.x);
+            animator.SetFloat("Vertical", velocity.y);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
+
+    void UpdateMovement()
+    {
+        rb.MovePosition(rb.position + velocity * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+}
